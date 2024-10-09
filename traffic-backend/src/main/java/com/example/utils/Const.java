52550f141380ee2a528d3513f49056a169a7a5f4
bjb <jiabao.bi@foxmail.com>
2024-10-09 21:24:34 +0800

    常量类

---
+++ traffic-backend/src/main/java/com/example/utils/Const.java
@@ -0,0 +1,12 @@
+package com.example.utils;
+
+/**
+ * 该类用于存储常量
+ */
+public class Const {
+    //返回成功的状态码
+    public static final int SUCCESS_CODE = 200;
+    //高德地图API相关
+    public static final String API_KEY = "8009868dd5506b4ca4a9e78b9a85b6e4";
+    public static final String BASE_URL = "https://restapi.amap.com/v3/place/text";
+}
