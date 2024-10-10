e02b7569f72ecc5f5a772ab4fde744e6d38abeb3
bjb <jiabao.bi@foxmail.com>
2024-10-10 17:26:13 +0800

    Element-plus导入

--- traffic-frontend/src/main.js
+++ traffic-frontend/src/main.js
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import axios from "axios";
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus';
 
 const app = createApp(App)
 
 axios.defaults.baseURL = 'http://localhost:8080'
 
 app.use(router)
+
+app.use(ElementPlus);
 
 app.mount('#app')
 
