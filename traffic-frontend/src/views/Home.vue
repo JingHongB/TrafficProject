01c5fcaa40090ba0f91a4552c32c3430216b784f
bjb <jiabao.bi@foxmail.com>
2024-10-10 16:22:15 +0800

    主页面

---
+++ traffic-frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import MapContainer from "@/components/MapContainer.vue";
+</script>
+
+<template>
+  <div id="map">
+    <MapContainer />
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
