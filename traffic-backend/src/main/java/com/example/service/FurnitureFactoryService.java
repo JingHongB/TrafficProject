7d8e2571e32d81da751c9df05e3f208ccee54803
bjb <jiabao.bi@foxmail.com>
2024-10-11 17:32:02 +0800

    修改

--- traffic-backend/src/main/java/com/example/service/FurnitureFactoryService.java
+++ traffic-backend/src/main/java/com/example/service/FurnitureFactoryService.java
@@ -1,11 +1,12 @@
 package com.example.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.model.entity.BaseLocationEntity;
 import com.example.model.entity.FurnitureFactory;
 
 import java.util.List;
 
-public interface FurnitureFactoryService {
+public interface FurnitureFactoryService extends IService<FurnitureFactory> {
     /**
      * 获取所有家具厂信息。
      *
--- traffic-backend/src/main/java/com/example/service/LumberyardService.java
+++ traffic-backend/src/main/java/com/example/service/LumberyardService.java
@@ -1,10 +1,12 @@
 package com.example.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.model.entity.BaseLocationEntity;
+import com.example.model.entity.Lumberyard;
 
 import java.util.List;
 
-public interface LumberyardService {
+public interface LumberyardService extends IService<Lumberyard> {
     /**
      * 获取所有伐木场信息。
      *
