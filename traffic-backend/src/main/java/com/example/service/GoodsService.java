cc6882019500a2249b29497347d4ae870da3d81f
bjb <jiabao.bi@foxmail.com>
2024-10-16 11:12:48 +0800

    货物流程更新

--- traffic-backend/src/main/java/com/example/service/GoodsService.java
+++ traffic-backend/src/main/java/com/example/service/GoodsService.java
@@ -3,8 +3,6 @@ package com.example.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.model.entity.Goods;
 
-import java.util.List;
-
 public interface GoodsService extends IService<Goods> {
     /**
      * 生成货物
