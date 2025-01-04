0704ce6974083b4bad044137d24270d3660f9a49
Gcwlkjm <3545972023@qq.com>
2025-01-04 17:20:39 +0800

    zjm-test

--- traffic-backend/src/main/java/com/example/TrafficBackendApplication.java
+++ traffic-backend/src/main/java/com/example/TrafficBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class TrafficBackendApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(TrafficBackendApplication.class, args);
     }
